b38640458c2947b09633c5d8a8a9957230ccf1d7
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-06-24 07:02:47 +0200

    Iteration  2+3 done

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Suppress the deprecation warning for `strictQuery`
+mongoose.set('strictQuery', true);
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Add the array of recipes from data.json
+    return Recipe.insertMany(data);
+  })
+  .then(recipes => {
+    // Print the title of each recipe
+    recipes.forEach(recipe => {
+      console.log(`Recipe created: ${recipe.title}`);
+    });
+    mongoose.connection.close(); // データベース接続を終了
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
