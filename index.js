fd082f276f694a196f4ce1e447016d6e05e5281c
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-06-24 07:28:47 +0200

    Iteration  4+5 test.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -15,18 +13,11 @@ mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
+    // Remove "Carrot Cake" recipe
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
   })
   .then(() => {
-    // Add the array of recipes from data.json
-    return Recipe.insertMany(data);
-  })
-  .then(recipes => {
-    // Print the title of each recipe
-    recipes.forEach(recipe => {
-      console.log(`Recipe created: ${recipe.title}`);
-    });
+    console.log('Successfully removed Carrot Cake');
     mongoose.connection.close(); // データベース接続を終了
   })
   .catch(error => {
